5dcb2b5be1f6bc1ef10a3cc68302e628fd0bbce3
Kostas Kokkoros <mavroprovato@gmail.com>
2022-08-08 01:17:33 +0300

    Improve fetching of data

--- fuelpricesgr/fetch.py
+++ fuelpricesgr/fetch.py
@@ -3,6 +3,7 @@
 import enum
 import logging
 import pathlib
+import re
 import shutil
 import sys
 
@@ -21,9 +22,9 @@ logger = logging.getLogger(__name__)
 class FuelData(enum.Enum):
     """Enumeration for the different types of fuel data.
     """
-    WEEKLY = 'deltia.view'
-    DAILY_COUNTRY = 'deltia_d.view'
-    DAILY_PREFECTURE = 'deltia_dn.view'
+    WEEKLY = 'deltia.view',
+    DAILY_COUNTRY = 'deltia_d.view',
+    DAILY_PREFECTURE = 'deltia_dn.view',
 
     def __init__(self, page: str):
         """Creates the enum.
@@ -41,12 +42,16 @@ def fetch_data():
         soup = bs4.BeautifulSoup(response.text, 'html.parser')
         for link in soup.find_all('a'):
             if link.has_attr('href') and link['href'].startswith('./files'):
-                file_path = DATA_PATH / link['href']
+                file_link = link['href'].replace(' ', '')
+                file_link = re.sub(r'\(\d\)', '', file_link)
+                file_link = re.sub(r'-\?+', '', file_link)
+                file_path = DATA_PATH / str(fuel_data) / link['href']
                 if not file_path.exists():
                     file_path.parent.mkdir(parents=True, exist_ok=True)
-                    logger.info(f"Downloading file %s", link['href'])
-                    file_url = f"{BASE_URL}/{link['href']}"
+                    file_url = f"{BASE_URL}/{file_link}"
+                    logger.info(f"Downloading file %s", file_url)
                     with requests.get(file_url, stream=True) as r, file_path.open('wb') as f:
+                        r.raise_for_status()
                         shutil.copyfileobj(r.raw, f)
 
 
