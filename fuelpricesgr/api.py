f276ca4924da0f4dd5cdbc7ff7e3d99b37248c30
Kostas Kokkoros <mavroprovato@gmail.com>
2022-08-29 05:17:33 +0300

    Move CORS allow origins and max days to settings

--- fuelpricesgr/api.py
+++ fuelpricesgr/api.py
@@ -7,17 +7,10 @@ import typing
 import fastapi
 import fastapi.middleware.cors
 
-from fuelpricesgr import database, enums
+from fuelpricesgr import database, enums, settings
 
 app = fastapi.FastAPI()
-app.add_middleware(
-    fastapi.middleware.cors.CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8080", "http://localhost:8080"],
-)
-
-
-# The maximum number of days to return from the API
-MAX_DAYS = 365
+app.add_middleware(fastapi.middleware.cors.CORSMiddleware, allow_origins=settings.CORS_ALLOW_ORIGINS)
 
 
 @app.get("/")
@@ -106,15 +99,15 @@ def get_date_range(start_date: datetime.date, end_date: datetime.date) -> typing
     # Make sure that we don't get more days than MAX_DAYS
     if start_date is None and end_date is None:
         end_date = datetime.date.today()
-        start_date = end_date - datetime.timedelta(days=MAX_DAYS)
+        start_date = end_date - datetime.timedelta(days=settings.MAX_DAYS)
     elif start_date is None:
-        start_date = end_date - datetime.timedelta(days=MAX_DAYS)
+        start_date = end_date - datetime.timedelta(days=settings.MAX_DAYS)
     elif end_date is None:
-        end_date = start_date + datetime.timedelta(days=MAX_DAYS)
+        end_date = start_date + datetime.timedelta(days=settings.MAX_DAYS)
     elif start_date > end_date:
         raise fastapi.HTTPException(status_code=400, detail="Start date must be before end date")
     else:
         days = (end_date - start_date).days
-        start_date = end_date - datetime.timedelta(days=min(days, MAX_DAYS))
+        start_date = end_date - datetime.timedelta(days=min(days, settings.MAX_DAYS))
 
     return end_date, start_date
--- fuelpricesgr/settings.py
+++ fuelpricesgr/settings.py
@@ -6,3 +6,8 @@ DATA_PATH = pathlib.Path(__file__).parent.parent / 'var'
 # The fetch URL
 FETCH_URL = 'http://www.fuelprices.gr'
 
+# CORS allow origins
+CORS_ALLOW_ORIGINS = ["http://127.0.0.1:8080", "http://localhost:8080"]
+
+# The maximum number of days to return from the API
+MAX_DAYS = 365
