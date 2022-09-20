6e713bb61e39718d222bb56f75f431b925ad637e
Kostas Kokkoros <mavroprovato@gmail.com>
2022-09-20 04:58:52 +0300

    Add configuration for building HTML file

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: 'src/index.html'
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
