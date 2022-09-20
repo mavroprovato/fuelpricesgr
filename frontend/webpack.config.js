7776fc06e900bf8a109ed344f24f315dd4d0fab2
Kostas Kokkoros <mavroprovato@gmail.com>
2022-09-20 05:16:54 +0300

    Add development configuration for webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
         template: 'src/index.html'
@@ -12,6 +17,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
