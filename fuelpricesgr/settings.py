8b6dd5ff5b020afd693e07fdb1b374b0ef94f561
Kostas Kokkoros <mavroprovato@gmail.com>
2022-09-22 01:40:30 +0300

    Add .env file for loading configuration

--- fuelpricesgr/settings.py
+++ fuelpricesgr/settings.py
@@ -1,18 +1,23 @@
 """Application settings module
 """
+import os
 import pathlib
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # The base data path
 DATA_PATH = pathlib.Path(__file__).parent.parent / 'var'
 
-# The fetch URL
+# The base URL from which to fetch the PDF data
 FETCH_URL = 'http://www.fuelprices.gr'
 
-# The timeout for requests in seconds
+# The timeout for fetching data in seconds
 REQUESTS_TIMEOUT = 5
 
 # CORS allow origins
-CORS_ALLOW_ORIGINS = ["http://127.0.0.1:8080", "http://localhost:8080"]
+CORS_ALLOW_ORIGINS = os.getenv('CORS_ALLOW_ORIGINS', 'http://127.0.0.1:8080,http://localhost:8080').split(',')
 
 # The maximum number of days to return from the API
 MAX_DAYS = 365
