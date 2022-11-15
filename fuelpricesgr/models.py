a455a80f6732c8ac35b5d305354de633ee8596c2
Kostas Kokkoros <mavroprovato@gmail.com>
2022-11-15 02:18:17 +0200

    Add indexes on dates

--- fuelpricesgr/models.py
+++ fuelpricesgr/models.py
@@ -9,7 +9,7 @@ class DailyCountry(models.Model):
     """The daily country database model
     """
     id = fields.IntField(pk=True)
-    date = fields.DateField()
+    date = fields.DateField(index=True)
     fuel_type = fields.CharEnumField(enum_type=enums.FuelType)
     number_of_stations = fields.IntField()
     price = fields.FloatField()
@@ -22,7 +22,7 @@ class DailyPrefecture(models.Model):
     """The daily prefecture database model
     """
     id = fields.IntField(pk=True)
-    date = fields.DateField()
+    date = fields.DateField(index=True)
     prefecture = fields.CharEnumField(enum_type=enums.Prefecture)
     fuel_type = fields.CharEnumField(enum_type=enums.FuelType)
     price = fields.FloatField()
@@ -35,7 +35,7 @@ class WeeklyCountry(models.Model):
     """The weekly country database model
     """
     id = fields.IntField(pk=True)
-    date = fields.DateField()
+    date = fields.DateField(index=True)
     fuel_type = fields.CharEnumField(enum_type=enums.FuelType)
     lowest_price = fields.FloatField()
     highest_price = fields.FloatField()
@@ -49,7 +49,7 @@ class WeeklyPrefecture(models.Model):
     """The weekly prefecture database model
     """
     id = fields.IntField(pk=True)
-    date = fields.DateField()
+    date = fields.DateField(index=True)
     fuel_type = fields.CharEnumField(enum_type=enums.FuelType)
     prefecture = fields.CharEnumField(enum_type=enums.Prefecture)
     lowest_price = fields.FloatField()
