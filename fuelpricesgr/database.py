3d0f2901f03b57a39685af1166fbb3afd8bfd05c
Kostas Kokkoros <mavroprovato@gmail.com>
2023-11-10 21:30:21 +0200

    [feat] create data directory if not exists

--- fuelpricesgr/database.py
+++ fuelpricesgr/database.py
@@ -1,8 +1,12 @@
 """The database module
 """
+import os
+
 import sqlalchemy.orm
 
 from fuelpricesgr import settings
+
+os.makedirs(settings.DATA_PATH, exist_ok=True)
 
 engine = sqlalchemy.create_engine(
     f"sqlite:///{(settings.DATA_PATH / 'db.sqlite')}", connect_args={"check_same_thread": False}, echo=settings.SHOW_SQL
