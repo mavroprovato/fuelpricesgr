b75bfad6d82e445e69371978773d93203ab251a7
Kostas Kokkoros <mavroprovato@gmail.com>
2024-01-03 23:49:24 +0200

    Fix tests

--- fuelpricesgr/tests.py
+++ fuelpricesgr/tests.py
@@ -11,7 +11,7 @@ client = TestClient(main.app)
 def test_index():
     """Test the index of the API.
     """
-    response = client.get("/")
+    response = client.get("/status")
     assert response.status_code == 200
     assert response.json() == {'cache_status': 'ok', 'db_status': 'ok'}
 
