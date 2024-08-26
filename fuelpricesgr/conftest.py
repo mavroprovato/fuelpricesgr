bfa60ba8379f6f0fae98b9ba8625bd6e9f3e0032
Kostas Kokkoros <mavroprovato@gmail.com>
2024-08-26 21:11:19 +0300

    Fix style

--- fuelpricesgr/conftest.py
+++ fuelpricesgr/conftest.py
@@ -4,10 +4,11 @@ import pathlib
 
 from fuelpricesgr import settings, storage
 
+
 def pytest_configure():
     """Configure pytest.
     """
     # Configure storage for testing
     settings.STORAGE_BACKEND = 'fuelpricesgr.storage.sql_alchemy.SqlAlchemyStorage'
-    settings.STORAGE_URL =  f"sqlite:///{pathlib.Path(__file__).parent.parent / 'var' / 'db_test.sqlite'}"
+    settings.STORAGE_URL = f"sqlite:///{pathlib.Path(__file__).parent.parent / 'var' / 'db_test.sqlite'}"
     storage.init_storage()
