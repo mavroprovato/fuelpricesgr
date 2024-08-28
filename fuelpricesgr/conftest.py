2b913c7ac616fd47f3bf2b1c695a17c3b92156c6
Kostas Kokkoros <mavroprovato@gmail.com>
2024-08-29 01:33:32 +0300

    Remove test data creation - does not work

--- fuelpricesgr/conftest.py
+++ fuelpricesgr/conftest.py
@@ -12,8 +12,3 @@ def pytest_configure():
     settings.STORAGE_BACKEND = 'fuelpricesgr.storage.sql_alchemy.SqlAlchemyStorage'
     settings.STORAGE_URL = f"sqlite:///{pathlib.Path(__file__).parent.parent / 'var' / 'db_test.sqlite'}"
     storage.init_storage()
-
-    # Create test data
-    from fuelpricesgr.tests.factories import sql_alchemy
-    sql_alchemy.WeeklyCountryFactory.create_batch(size=1000)
-    sql_alchemy.DailyCountryFactory.create_batch(size=1000)
