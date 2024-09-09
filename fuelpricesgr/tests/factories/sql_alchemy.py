69253608ba46ffcf7def0aa379163e8f5235a7ae
Kostas Kokkoros <mavroprovato@gmail.com>
2024-09-09 22:34:37 +0300

    Reduce duplication on factories

--- fuelpricesgr/tests/factories/sql_alchemy.py
+++ fuelpricesgr/tests/factories/sql_alchemy.py
@@ -7,7 +7,15 @@ from fuelpricesgr.tests import common
 import fuelpricesgr.storage.sql_alchemy
 
 
-class WeeklyCountryFactory(factory.alchemy.SQLAlchemyModelFactory):
+class BaseDataFactory(factory.alchemy.SQLAlchemyModelFactory):
+    """The base data factory
+    """
+    date = factory.Faker('past_date', start_date='-1y')
+    fuel_type = factory.Faker('enum', enum_cls=enums.FuelType)
+    price = factory.Faker('pydecimal', right_digits=3, min_value=0.5, max_value=2.5)
+
+
+class WeeklyCountryFactory(BaseDataFactory):
     """The weekly country data factory
     """
     class Meta:
@@ -15,13 +23,10 @@ class WeeklyCountryFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session = common.Session
         sqlalchemy_get_or_create = ('date', 'fuel_type')
 
-    date = factory.Faker('past_date', start_date='-1y')
-    fuel_type = factory.Faker('enum', enum_cls=enums.FuelType)
-    price = factory.Faker('pydecimal', right_digits=3, min_value=0.5, max_value=2.5)
     number_of_stations = factory.Faker('pyint', min_value=0, max_value=1000)
 
 
-class WeeklyPrefectureFactory(factory.alchemy.SQLAlchemyModelFactory):
+class WeeklyPrefectureFactory(BaseDataFactory):
     """The weekly prefecture data factory
     """
     class Meta:
@@ -29,13 +34,10 @@ class WeeklyPrefectureFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session = common.Session
         sqlalchemy_get_or_create = ('date', 'prefecture', 'fuel_type')
 
-    date = factory.Faker('past_date', start_date='-1y')
-    fuel_type = factory.Faker('enum', enum_cls=enums.FuelType)
     prefecture = factory.Faker('enum', enum_cls=enums.Prefecture)
-    price = factory.Faker('pydecimal', right_digits=3, min_value=0.5, max_value=2.5)
 
 
-class DailyCountryFactory(factory.alchemy.SQLAlchemyModelFactory):
+class DailyCountryFactory(BaseDataFactory):
     """The daily country data factory
     """
     class Meta:
@@ -43,13 +45,10 @@ class DailyCountryFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session = common.Session
         sqlalchemy_get_or_create = ('date', 'fuel_type')
 
-    date = factory.Faker('past_date', start_date='-1y')
-    fuel_type = factory.Faker('enum', enum_cls=enums.FuelType)
-    price = factory.Faker('pydecimal', right_digits=3, min_value=0.5, max_value=2.5)
     number_of_stations = factory.Faker('pyint', min_value=0, max_value=1000)
 
 
-class DailyPrefectureFactory(factory.alchemy.SQLAlchemyModelFactory):
+class DailyPrefectureFactory(BaseDataFactory):
     """The daily prefecture data factory
     """
     class Meta:
@@ -57,7 +56,4 @@ class DailyPrefectureFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session = common.Session
         sqlalchemy_get_or_create = ('date', 'prefecture', 'fuel_type')
 
-    date = factory.Faker('past_date', start_date='-1y')
-    fuel_type = factory.Faker('enum', enum_cls=enums.FuelType)
     prefecture = factory.Faker('enum', enum_cls=enums.Prefecture)
-    price = factory.Faker('pydecimal', right_digits=3, min_value=0.5, max_value=2.5)
