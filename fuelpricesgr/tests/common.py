5b43f57bb5700d1afdc5cacf717e059c2a0a37e3
Kostas Kokkoros <mavroprovato@gmail.com>
2026-05-26 23:15:06 +0300

    Test the status endpoint

--- fuelpricesgr/tests/common.py
+++ fuelpricesgr/tests/common.py
@@ -1,10 +1,64 @@
 """Common test module
 """
+import datetime
+from typing import Iterable
 import unittest
 
 import fastapi.testclient
 
-from fuelpricesgr import main
+from fuelpricesgr import enums, main, models, storage
+
+
+def mock_data_storage(status: enums.ApplicationStatus = enums.ApplicationStatus.OK):
+    """Mock the storage backend.
+
+    :param status: The application status.
+    """
+    class TestStorage(storage.BaseStorage):
+        """Storage class for testing"""
+
+        def status(self) -> enums.ApplicationStatus:
+            return status
+
+        def date_range(self, data_type: enums.DataType) -> models.DateRange | None:
+            raise NotImplementedError()
+
+        def weekly_country_data(
+                self, start_date: datetime.date, end_date: datetime.date
+        ) -> Iterable[models.DatePriceNumberOfStationsData]:
+            raise NotImplementedError()
+
+        def weekly_prefecture_data(
+            self, prefecture: enums.Prefecture, start_date: datetime.date, end_date: datetime.date
+        ) -> Iterable[models.DatePriceData]:
+            raise NotImplementedError()
+
+        def daily_country_data(
+                self, start_date: datetime.date, end_date: datetime.date
+        ) -> Iterable[models.DatePriceNumberOfStationsData]:
+            raise NotImplementedError()
+
+        def daily_prefecture_data(
+                self, prefecture: enums.Prefecture, start_date: datetime.date, end_date: datetime.date
+        ) -> Iterable[models.DatePriceData]:
+            raise NotImplementedError()
+
+        def data_exists(self, data_type: enums.DataType, date: datetime.date) -> bool:
+            raise NotImplementedError()
+
+        def update_data(self, date: datetime.date, data_type: enums.DataType, data: list[dict]):
+            raise NotImplementedError()
+
+        def create_user(self, email: str, password: str, admin: bool = False):
+            raise NotImplementedError()
+
+        def get_user(self, email: str):
+            raise NotImplementedError()
+
+        def get_admin_user_emails(self) -> list[str]:
+            raise NotImplementedError()
+
+    return TestStorage()
 
 
 class BaseAPITestCase(unittest.TestCase):
--- fuelpricesgr/tests/test_api/test_status.py
+++ fuelpricesgr/tests/test_api/test_status.py
@@ -1,6 +1,9 @@
 """Test the status endpoint
 """
-from fuelpricesgr import tests
+from fuelpricesgr import enums, main, tests, views
+
+from .. import common
+
 
 class StatusTestCase(tests.common.BaseAPITestCase):
     """The status endpoint test case
@@ -8,7 +11,21 @@ class StatusTestCase(tests.common.BaseAPITestCase):
     def test_status(self):
         """The status endpoint test case
         """
+        main.app.dependency_overrides[views.api.get_storage] = lambda: common.mock_data_storage(
+            status=enums.ApplicationStatus.OK
+        )
         response = self.client.get('/api/status')
 
         assert response.status_code == 200
         assert response.json() == {'cache_status': 'ok', 'db_status': 'ok'}
+
+    def test_status_storage_failure(self):
+        """The status endpoint test case in case of storage failure
+        """
+        main.app.dependency_overrides[views.api.get_storage] = lambda: common.mock_data_storage(
+            status=enums.ApplicationStatus.ERROR
+        )
+        response = self.client.get('/api/status')
+
+        assert response.status_code == 200
+        assert response.json() == {'cache_status': 'ok', 'db_status': 'error'}
