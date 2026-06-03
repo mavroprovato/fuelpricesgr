1e813b020707e694f648c6b0476f76cebec4ec44
Kostas Kokkoros <mavroprovato@gmail.com>
2026-06-03 06:45:41 +0300

    Add dates for daily country enum and daily prefecture enum

--- fuelpricesgr/tests/test_enums.py
+++ fuelpricesgr/tests/test_enums.py
@@ -9,11 +9,29 @@ from fuelpricesgr import enums
 class EnumsTestCase(unittest.TestCase):
     """The enums test case.
     """
-    def test_weekly_dates(self):
-        """Test that the dates for the weekly data are correct.
+    def test_data_file_type_weekly_dates(self):
+        """Test that the dates for the weekly data file type are correct.
         """
         dates = list(enums.DataFileType.WEEKLY.dates(datetime.date(2026, 5, 1), datetime.date(2026, 5, 31)))
         self.assertSequenceEqual(dates, (
             datetime.date(2026, 5, 1), datetime.date(2026, 5, 8), datetime.date(2026, 5, 15),
             datetime.date(2026, 5, 22), datetime.date(2026, 5, 29)
+        ))
+
+    def test_data_file_type_daily_country_dates(self):
+        """Test that the dates for the daily prefecture data file type are correct.
+        """
+        dates = list(enums.DataFileType.DAILY_COUNTRY.dates(datetime.date(2026, 5, 1), datetime.date(2026, 5, 5)))
+        self.assertSequenceEqual(dates, (
+            datetime.date(2026, 5, 1), datetime.date(2026, 5, 2), datetime.date(2026, 5, 3), datetime.date(2026, 5, 4),
+            datetime.date(2026, 5, 5)
+        ))
+
+    def test_data_file_type_daily_prefecture_dates(self):
+        """Test that the dates for the daily prefecture data file type are correct.
+        """
+        dates = list(enums.DataFileType.DAILY_PREFECTURE.dates(datetime.date(2026, 5, 10), datetime.date(2026, 5, 14)))
+        self.assertSequenceEqual(dates, (
+            datetime.date(2026, 5, 10), datetime.date(2026, 5, 11), datetime.date(2026, 5, 12),
+            datetime.date(2026, 5, 13), datetime.date(2026, 5, 14)
         ))
