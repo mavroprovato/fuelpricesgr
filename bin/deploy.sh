495d596e42ade91479cd3382c4dcb048701a5d41
Kostas Kokkoros <mavroprovato@gmail.com>
2026-07-10 00:46:23 +0300

    Check input parameters for the deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,6 +5,11 @@ set -o errexit
 set -o errtrace
 set -o pipefail
 
+if [ "$#" -ne 2 ]; then
+    echo "Usage: deploy.sh <key> <server>"
+    exit
+fi
+
 ssh -i "$1" "$2" 'su - app -c "cd /home/app/apps/fuelpricesgr; git fetch origin master"'
 ssh -i "$1" "$2" 'su - app -c "cd /home/app/apps/fuelpricesgr; git merge"'
 ssh -i "$1" "$2" 'systemctl restart fuelpricesgr-api.service'
