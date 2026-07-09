96aa6dd25c06ddfd41ae987c9800b19b1c8f715b
Kostas Kokkoros <mavroprovato@gmail.com>
2026-07-10 00:42:18 +0300

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,5 +5,6 @@ set -o errexit
 set -o errtrace
 set -o pipefail
 
-ssh -i "$1" "$2" 'su - app -c "cd /home/app/projects/fuelpricesgr; git pull"'
-ssh -i "$1" "$2" 'systemctl restart fuelpricesgr.service'
+ssh -i "$1" "$2" 'su - app -c "cd /home/app/apps/fuelpricesgr; git fetch origin master"'
+ssh -i "$1" "$2" 'su - app -c "cd /home/app/apps/fuelpricesgr; git merge"'
+ssh -i "$1" "$2" 'systemctl restart fuelpricesgr-api.service'
